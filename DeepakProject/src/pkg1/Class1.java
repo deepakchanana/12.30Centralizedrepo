650fd647c4b9f2898a5461466fdcce75f291465f
deepakchanana <deepakdeep1992@gmail.com>
2020-12-27 11:57:28 -0800

    Pushing the code first time

---
+++ DeepakProject/src/pkg1/Class1.java
@@ -0,0 +1,21 @@
+package pkg1;
+
+public class Class1
+{
+  public void method1()
+  {
+	  System.out.println("Deepak project1");
+  }
+  public void method2()
+  {
+	  System.out.println("Deepak project2");
+  }
+  public void method3()
+  {
+	  System.out.println("Deepak project3");
+  }
+  public void method4()
+  {
+	  System.out.println("Deepak project4");
+  }
+}
